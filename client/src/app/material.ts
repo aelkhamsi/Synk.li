c5e5cbe8b8ef4c5064d45e6b2d996c5a8128b87e
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2020-06-29 09:06:14 +0200

    Creating a test login box

--- client/src/app/material.ts
+++ client/src/app/material.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSnackBarModule} from '@angular/material';
+import { MatSelectModule} from '@angular/material/select';
+
 
 const modules = [
     MatButtonModule,
@@ -12,7 +14,9 @@ const modules = [
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatSelectModule,
+    MatCheckboxModule,
 ];
 
 @NgModule({
@@ -20,3 +24,4 @@ const modules = [
     exports: [...modules]
 })
 export class MaterialModule {}
+
--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginPage } from './pages/login/login.page';
 import { SignupPage } from './pages/signup/signup.page';
 import { HeaderComponent } from './components/header/header.component';
+import { TestComponent } from './components/test/test.component';
+
+
 
 @NgModule({
   declarations: [
     LoginPage, 
     SignupPage, 
-    HeaderComponent
+    HeaderComponent, 
+    TestComponent
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     AuthRoutingModule
   ]
 })
