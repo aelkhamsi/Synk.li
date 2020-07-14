6a6078b5c244cb27e5628d2820762a64fdfeef00
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2020-07-14 02:22:47 +0200

    Build formGroup for signup form && Build validation

--- client/src/app/modules/auth/components/signup-box/signup-box.component.ts
+++ client/src/app/modules/auth/components/signup-box/signup-box.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MustMatch } from '../../_helpers/must-match.validator';
 
 @Component({
   selector: 'app-signup-box',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupBoxComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  fieldTextType: Boolean = false;
+  formSubmitted: Boolean = false;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+      username: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ["", Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    })
   }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.signupForm.invalid) return;
+    console.log(this.signupForm.value);
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.signupForm.controls; }
 
 }
