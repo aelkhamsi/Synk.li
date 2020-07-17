a4a90c83c5193336afaaab4f0ae23725a57e3849
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2020-07-17 22:43:18 +0200

    Create Form create-room & join-room of /dashboard page

--- client/src/app/modules/main/main.module.ts
+++ client/src/app/modules/main/main.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MainRoutingModule } from './main-routing.module';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
+import { HeaderComponent } from './components/header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    DashboardPage
+    DashboardPage,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ]
 })
 export class MainModule { }
--- client/src/app/modules/main/pages/dashboard/dashboard.page.ts
+++ client/src/app/modules/main/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -9,17 +12,40 @@ import { Router } from '@angular/router';
 })
 export class DashboardPage implements OnInit {
 
+  createRoomForm: FormGroup;
+  joinRoomForm: FormGroup;
+  formSubmitted: Boolean = false;
+  numericalRegex: string = "[0-9]*";
+
   constructor(
+    private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
   ) { }
 
   ngOnInit() {
+    this.joinRoomForm = this.fb.group({
+      roomId: ["", [Validators.required, Validators.pattern(this.numericalRegex)]]
+    })
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['home']);
   } 
+
+
+  onCreate(): void {
+    console.log('skr');
+  }
+
+  onJoin(): void {
+    this.formSubmitted = true;
+    if (this.joinRoomForm.invalid) return;
+    console.log('brr');
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.joinRoomForm.controls; }
 
 }
